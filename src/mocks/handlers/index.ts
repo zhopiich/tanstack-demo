e2978ac8bcaa3da13c02d54c441317a854c6e6bc
zhopiich <zhoping9449@gmail.com>
2026-04-20 22:32:21 +0800

    chore(mock): add global API delay to msw handlers

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,8 +1,14 @@
+import { delay, http } from 'msw'
 import { authHandlers } from './auth'
 import { dashboardHandlers } from './dashboard'
 import { submissionHandlers } from './submissions'
 
+const delayHandler = http.all('/api/*', async () => {
+  await delay('real')
+})
+
 export const handlers = [
+  delayHandler,
   ...authHandlers,
   ...submissionHandlers,
   ...dashboardHandlers,
