8cbb37893236b785d27b8fe5be3a828341b42e7c
zhopiich <zhoping9449@gmail.com>
2026-04-21 19:27:00 +0800

    feat: add ConfirmDialog component

---
+++ src/components/ConfirmDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <AlertDialog :open="open" @update:open="$emit('update:open', $event)">
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>{{ title }}</AlertDialogTitle>
+        <AlertDialogDescription>{{ description }}</AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction @click="$emit('confirm')">
+          Delete
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
+
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog'
+
+defineProps<{
+  open: boolean
+  title: string
+  description: string
+}>()
+
+defineEmits<{
+  'update:open': [value: boolean]
+  'confirm': []
+}>()
+</script>
